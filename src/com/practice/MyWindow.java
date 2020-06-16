d581f2799e4b2a55a6578398180078e7444d3833
sushanbaniya <sushanbaniya@gmail.com>
2020-06-16 10:45:05 +0545

    First Commit

---
+++ src/com/practice/MyWindow.java
@@ -0,0 +1,68 @@
+package com.practice;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+import javax.swing.*;
+public class MyWindow extends JFrame {
+	
+	
+	JLabel heading;
+	
+	JLabel clockLabel;
+	
+	Font font = new Font("", Font.BOLD, 35);
+	
+	MyWindow(){
+		
+		 
+		
+		
+		
+		super.setTitle("My Clock");
+		super.setSize(400,500);
+		super.setLocation(300,50);
+		this.createGUI();
+		this.startClock();
+		super.setVisible(true);
+		
+		
+	}
+	public void createGUI(){
+		
+		heading = new JLabel("My Clock");
+		clockLabel = new JLabel("Clock");
+	
+	
+		heading.setFont(font);
+		clockLabel.setFont(font);
+		
+		this.setLayout(new GridLayout(2,1));
+		this.add(heading);
+		this.add(clockLabel);
+		
+		
+		
+	}
+	
+	public void startClock() {
+		Timer timer = new Timer(1000, new ActionListener()
+				{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			String dateTime = new Date().toString();
+			clockLabel.setText(dateTime);
+		}
+		
+	});
+	
+	timer.start();
+	
+	}
+
+	
+
+}
